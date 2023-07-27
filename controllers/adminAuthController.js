be9da7dc66d79113d6e0d233d4d742bc418ed178
Satyam Tripathi <98680993+satyacasm@users.noreply.github.com>
2023-07-27 11:31:02 +0530

    Update adminAuthController.js

--- controllers/adminAuthController.js
+++ controllers/adminAuthController.js
@@ -8,11 +8,7 @@ const mysql=require('mysql2');
 require('dotenv').config()
 // Authentication middleware
 
-const conn=mysql.createConnection({
-  host:"localhost",
-  user:process.env.USER,
-  password:process.env.PASSWORD
-});
+const conn=mysql.createConnection(process.env.DATABASE_URL);
 conn.connect((err)=>{
   if(err) throw err;
   console.log("Connected to MySQL Server");
